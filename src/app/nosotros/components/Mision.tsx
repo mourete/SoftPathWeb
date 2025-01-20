fd37ce2107b80133d9f55987fd1b7c5c2a7fb094
karlagn96 <karlaivonnegn@gmail.com>
2025-01-20 12:40:58 -0600

    se añadio cambio en la parte de nosotros, ya quedo concluida esa parte de la pestaña

--- src/app/nosotros/components/Mision.tsx
+++ src/app/nosotros/components/Mision.tsx
@@ -1,26 +1,31 @@
 const MissionVision: React.FC = () => {
     return (
-      <section className="py-16 max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-8">
-        <div>
+      <section className="py-16 max-w-3xl mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-8">
+        <div className="relative">
           <img
             src="/imagen5_nosotros.png"
             alt="Misión"
             className="rounded-lg shadow-md"
           />
+          <div className="absolute bottom-0 right-0 bg-lime-500 text-white text-center p-4 rounded shadow-lg">
+          <p className="text-2xl font-bold">+10</p>
+          <p className="text-sm">Años de experiencia</p>
         </div>
+        </div>
+
         <div>
-          <h2 className="text-2xl font-bold text-blue-500">Somos especialistas en gestión de riesgos y datos</h2>
+          <h2 className="text-1xl font-bold text-blue-500">Somos especialistas en gestión de riesgos y datos</h2>
           <div className="mt-6">
-            <h3 className="text-xl font-semibold text-black">Misión</h3>
-            <p className="mt-4 text-gray-600 text-justify">
+            <h3 className="text-xl font-bold text-black">Misión</h3>
+            <p className=" mt-2 text-sm text-gray-700 text-justify">
             Ofrecemos soluciones especializadas para identificar, evaluar y mitigar riesgos en organizaciones. 
             Guiamos a nuestros clientes hacia decisiones informadas, minimizando riesgos y maximizando 
             oportunidades en cumplimiento con la legalidad y estándares de calidad.
             </p>
           </div>
           <div className="mt-6">
-            <h3 className="text-xl font-semibold text-black">Visión</h3>
-            <p className="mt-2 text-gray-600 text-justify">
+            <h3 className="text-xl font-bold text-black">Visión</h3>
+            <p className="mt-2 text-sm text-gray-700 text-justify">
             Ser líderes nacionales en análisis de datos y mitigación de riesgos. 
             Brindamos soluciones innovadoras para resguardar la continuidad operativa de las organizaciones y garantizar su seguridad a largo plazo.
             </p>
